73a9c2e01d629ba4423553595237f7fc0c3ac160
tekiteki <tekiteki@naver.com>
2020-10-19 17:41:52 +0900

    area, item

--- admin/js/main.js
+++ admin/js/main.js
@@ -6,6 +6,8 @@
     var today = new Date();
     var sDate = new Date(new Date().setDate(new Date().getDate() - START_DATE_GAP));
     var eDate = today;
+    var area = '';
+    var item = '';
 
     function toStringDate(date) {
         var yyyy = date.getFullYear();
@@ -13,13 +15,69 @@
         mm = mm >= 10 ? mm : '0' + mm;
         var dd = date.getDate();
         dd = dd >= 10 ? dd : '0' + dd;
-        return yyyy + "-" + mm + "-" + dd;
+        return yyyy + "-" + mm + "-" + dd; 
     };
 
+    var dummyData = [
+        { 'date': '2020-10-10', 'area': '영역1', 'item': '광고물1번', 'status': 'normal', 'imp': 20000, 'clicks': 12 },
+        { 'date': '2020-10-08', 'area': '영역2', 'item': '광고물2번', 'status': 'normal', 'imp': 30000, 'clicks': 10 },
+        { 'date': '2020-10-09', 'area': '영역3', 'item': '광고물2번', 'status': 'normal', 'imp': 12312300, 'clicks': 1120 }
+    ];
+
+    var dummyArealist = [
+        {'area':'영역1'},
+        {'area':'영역2'},
+        {'area':'영역3'}
+    ]
+
+    var dummyAdItemlist = [
+        {'item':'광고물1번'},
+        {'item':'광고물2번'},
+        {'item':'광고물3번'}
+    ]
+
+    function buildAreaList(list) {
+        var areaList = document.getElementById('areaList');
+        for(var i=0; i < list.length; i++) {
+            var li = `<a href="#">
+            <li class="my-2 areas" value="${list[i].area}">${list[i].area}</li>
+            </a>`;
+            areaList.innerHTML += li;
+        }
+    }
+
+    function buildAdItemList(list) {
+        var adItemList = document.getElementById('adItemList');
+        for(var i=0; i < list.length; i++) {
+            var li = `<a href="#">
+            <li class="my-2 items">${list[i].item}</li>
+            </a>`;
+            adItemList.innerHTML += li;
+        }
+    }
+
+    function bulidTable(data) {
+        var table = document.getElementById('tableBody');
+        for (var i = 0; i < data.length; i++) {
+            var row = `<tr>
+              <td class="text-center">${data[i].date}</td>
+              <td class="text-center">${data[i].area}</td>
+              <td class="text-center">${data[i].item}</td>
+              <td class="text-center">${data[i].status}</td>
+              <td class="text-right">${(data[i].imp).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+              <td class="text-right">${(data[i].clicks).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</td>
+              <td class="text-right">${((data[i].clicks / data[i].imp) * 100).toFixed(3) + "%"}</td>
+              </tr>`;
+              table.innerHTML += row;
+        }
+    }
 
     $(function () {
 
-        // 날짜 초기화
+        buildAreaList(dummyArealist);
+        buildAdItemList(dummyAdItemlist);
+        bulidTable(dummyData);
+
         $('#startDateValue').text(toStringDate(sDate));
         $('#startDatePicker').val(toStringDate(sDate));
         $('#endDateValue').text(toStringDate(eDate));
@@ -73,23 +131,74 @@
             $('#endDatePicker').val(toStringDate(today));
         });
 
-        // submit
-        $('#submitBox a').on('click', function () {
-            alert('적용');
-        });
-
+        // Start DatePicker
         $('#startDatePicker').datepicker({
-            format: 'yyyy-mm-dd'
-        }).on('hide', function (event) {
+            format: 'yyyy-mm-dd',
+            autoclose: true,
+            todayHighlight: true
+        })
+        .on('hide', function (event) {
             event.preventDefault();
             event.stopPropagation();
+        })
+        .on('changeDate', function(event){
+            sDate = event.date
+            $('#startDateValue').text(toStringDate(sDate));           
         });
 
+
+        // End DatePicker
         $('#endDatePicker').datepicker({
-            format: 'yyyy-mm-dd'
+            format: 'yyyy-mm-dd',
+            autoclose: true,
+            todayHighlight: true
         }).on('hide', function (event) {
             event.preventDefault();
             event.stopPropagation();
+        })
+        .on('changeDate', function(event){
+            eDate = event.date;
+            $('#endDateValue').text(toStringDate(eDate));                                   
+        })
+        ;
+
+        // Area List
+        $('.areas').on('click', function(){
+            $('#areaValue').text($(this).text());
+            $('#areaModal').modal('hide');
+            area = $(this).text();
         });
+
+        // Ad Item List
+        $('.items').on('click', function(){
+            $('#adItemValue').text($(this).text());
+            $('#adItemModal').modal('hide');
+            item = $(this).text();
+        });
+
+        
+        // Submit
+        $('#submitBox a').on('click', function () {
+            // Validate Date
+            if($('#startDatePicker').val() > $('#endDatePicker').val()) {
+                alert('기간을 확인해 주세요')
+                return false;
+            } 
+            // Validate Area
+            else if(area == '') {
+                alert('영역을 선택해 주세요');
+                return false;
+            } 
+            else if(item == '') {
+                alert('광고물을 선택해 주세요');
+                return false;
+            }
+             else  {
+                alert('good!');
+                $('#tableBody').empty();
+                bulidTable(dummyData);
+            }
+        });
+
     });
 })(jQuery);
